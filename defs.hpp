3b55282137aae0db2ba13d01ac60fab1a6cb85c7
Marilia <marilia.araujo@academico.ifpb.edu.br>
2022-08-21 16:47:24 -0300

    instruções

---
+++ defs.hpp
@@ -0,0 +1,13 @@
+//
+// Created by marilia on 21/08/22.
+//
+
+#ifndef NOVO_SUAP_2_DEFS_H
+#define NOVO_SUAP_2_DEFS_H
+#include "includes.h"
+#include <iostream>
+
+void adiciona_profesor(){
+
+}
+#endif //NOVO_SUAP_2_DEFS_H
--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
 //
 // Created by marilia on 17/08/22.
 //
-#include "aluno.h"
-#include "aluno_ensino_medio.h"
-#include "aluno_superior/aluno_superior.h"
-#include "professor/professor.h"
-#include "materia/materia.h"
-#include "boletim/boletim.h"
-#include "cursos/cursos.h"
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "includes.h"
 using std::cout;
-
+using std::endl;
+using std::cin;
 
 
 int main(){
@@ -58,18 +50,39 @@ int main(){
     std::cout<<"▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"<< std::endl;
     std::cout<<""<< std::endl;
 
+    cout << "=============================================================================================="<< endl;
+    cout << "----------> [0] cadastro de administrador"<< endl;
+    cout << "----------> [1] cadastro de professor"<< endl;
+    cout << "----------> [2] cadastro de aluno"<< endl;
+    cout << "----------> [3] fazer login aluno"<< endl;
+    cout << "----------> [4] fazer login professor"<< endl;
+    cout << "----------> [5] ver o menu novamente"<< endl;
+    cout << "=============================================================================================="<< endl;
+    int var;
+    cin >> var;
+
+    switch (var) {
+        case 0:
+            cout<< "oxe";
+            break;
+    }
+
+
+
     std::cout<< "Criando um aluno do ensino superior no SUAP2:" << std::endl;
     std::cout << "digite o nome do aluno: ";
-    std::cin >> aluno1.nome_aluno;
+    std::getchar();
+    std::getline (std::cin,aluno1.nome_aluno);
     std::cout << "digite a data de nascimento, como o moedelo '12/12/1212': ";
     std::cin >> aluno1.data_nascimento_aluno;
     boletim alana;
     alana.materias.push_back(POO);
 
-
     std::cout<< "olá, " << aluno1.nome_aluno << " bem vindo(a) ao BPFI, você esta matriculado"
-      <<" no curso de Engenharia da Computação, seu email será:"<<aluno_superior::cria_email_academico(aluno1.nome_aluno) << std::endl;
+      <<" no curso de Engenharia da Computação, seu email será:"<<
+      aluno_superior::cria_email_academico(aluno1.nome_aluno) << std::endl;
     alana.print_boletim();
 
     return 0;
 }
+
